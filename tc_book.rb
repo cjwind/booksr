60b411736eaa596ad9e8d16fc3cd5f9daf1710ca
cjwind <cwentsai@gmail.com>
2015-01-08 21:15:34 +0800

    complete unit test of getting function of Book

--- tc_book.rb
+++ tc_book.rb
@@ -17,34 +17,35 @@ class TestBook < Test::Unit::TestCase
 	end
 
 	def test_authors
-
+		assert_equal(1, @book.authors.size)
+		assert_equal("吳曉樂", @book.authors[0])
 	end
 
 	def test_publisher
-
+		assert_equal("英屬蓋曼群島商網路與書出版股份有限公司臺灣分公司", @book.publisher)
 	end
 
 	def test_published_date
-
+		assert_equal("2014-11-01", @book.published_date)
 	end
 
 	def test_description
-
+		assert_equal("沒有一篇是普羅大眾樂見的教育神話。 沒有一篇看了會感到喜悅。 沒有一篇看了心中不會亂糟糟的，甚至覺得煩。 然而，這些事情確實發生過。 不僅確實發生過，極可能仍在發生⋯⋯ 說故事的人，是個證人。她花了七年時間，打開一扇又一扇的大門，走進不同的家庭。在門的背後，她見證了各色光怪陸離的景象：一個每日給兒子準備雞精、維他命的母親，在收到兒子成績單的當下，卻也毫不猶豫地甩出一記耳光；為了安撫雙親，躲在衣櫃裡欺騙別人也傷害自己的兒子；也或許，她看見一種用鈔票堆疊起來的親情⋯⋯她以為她是家教，只需帶給學生知識，沒想到學生及他們的家庭帶給她更多的衝擊。 什麼是教育的本質？愛是有條件的嗎？ 本書沒有給家長教條式的叮嚀與建議，只有一個個震撼人心的故事。這些故事之所以存在，是期待我們去凝視一個初衷，靜下來，好好想想，把小孩帶到這世界上的初衷。 ■ 被考試綁架的家庭故事——《你的孩子不是你的孩子》新書座談會 講者：吳曉樂（本書作者） 時間：2014年11月29日（六）11:00~12:00 地點：水牛書店 ■ 每個孩子都是獨立的個體——《你的孩子不是你的孩子》新書座談會 講者：吳曉樂（本書作者）、唐光華（樂觀書院創辦人） 時間：2014年12月4日（四）19:30~21:00 地點：誠品信義店3F Mini-Forum", @book.description)
 	end
 
 	def test_isbn10
-
+		assert_equal("9866841596", @book.isbn10)
 	end
 
 	def test_isbn13
-
+		assert_equal("9789866841590", @book.isbn13)
 	end
 
 	def test_page_count
-
+		assert_equal(328, @book.page_count)
 	end
 
-	def test_language
-
+	def test_lang
+		assert_equal("zh-TW", @book.lang)
 	end
 end
