79ec1b9a05dae62281c8fdce153ffbe72e9d0e90
cjwind <cwentsai@gmail.com>
2015-01-09 22:31:48 +0800

    refine code

--- booksr.rb
+++ booksr.rb
@@ -2,18 +2,8 @@ require './api_handler'
 
 class Booksr
 	def self.search(query_string, query_type, query_sources)
-		if query_type == :title
-			
-		elsif query_type == :author
-			api = ApiHandler.new
-			books = api.search_by_google(query_string, query_type)
-		elsif query_type == :isbn
-			api = ApiHandler.new
-			books = api.search_by_google(query_string, query_type)
-		elsif query_type == :keyword
-
-		end
-
+		api = ApiHandler.new
+		books = api.search_by_google(query_string, query_type)
 		return books
 	end
 end
