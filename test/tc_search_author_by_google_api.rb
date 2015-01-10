2e7b8953e040924338bf29e2895868cef2a9ee37
cjwind <cwentsai@gmail.com>
2015-01-10 10:57:16 +0800

    update test

--- test/tc_search_author_by_google_api.rb
+++ test/tc_search_author_by_google_api.rb
@@ -4,11 +4,30 @@ require 'test/unit'
 require './booksr'
 require './book'
 
-class TestSearchAuthorByGoogleApi < Test::Unit::TestCase
+class TestSearchAuthorByGoogleApi1 < Test::Unit::TestCase
 	def setup	# will be called before run each member function
+		@books = Booksr.search("娥蘇拉勒瑰恩", :author, [:google])
+	end
+
+	def test_result_count
+		assert_equal(5, @books.size)
+	end
+
+	def test_results
+		assert_equal("地海傳說: 地海奇風", @books[0].title)
+		assert_equal("地海傳說: 地海孤雛", @books[1].title)
+		assert_equal("地海傳說: 地海巫師", @books[2].title)
+		assert_equal("地海傳說: 地海故事集", @books[3].title)
+		assert_equal("地海傳說: 地海古墓", @books[4].title)
+	end
+end
+
+class TestSearchAuthorByGoogleApi2 < Test::Unit::TestCase
+	def setup
 		@books = Booksr.search("侯文詠", :author, [:google])
 	end
 
 	def test_result_count
+		assert_equal(59, @books.size)
 	end
 end
