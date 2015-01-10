01bd6d56dc05029cc6345d1fbf03a30b985c4eba
cjwind <cwentsai@gmail.com>
2015-01-10 14:50:46 +0800

    Add feature of search title by google

--- api_handler.rb
+++ api_handler.rb
@@ -12,7 +12,18 @@ class ApiHandler
 		parser = Parser.new
 
 		if query_type == :title
+			title = URI::escape(query_string)
+			start_index = 0
 
+			begin
+				query = "?q=intitle:#{title}&startIndex=#{start_index}&maxResults=#{GOOGLE_MAX_RESULTS}"
+				
+				response = RestClient.get api_uri + query
+				responses.push(response)
+				
+				data = parser.parse_json(response)
+				start_index += GOOGLE_MAX_RESULTS
+			end while data.size == GOOGLE_MAX_RESULTS
 		elsif query_type == :author
 			author = URI::escape(query_string)
 			start_index = 0
@@ -29,7 +40,7 @@ class ApiHandler
 		elsif query_type == :isbn
 			isbn = query_string
 			query = "?q=isbn:#{isbn}"
-			
+
 			response = RestClient.get api_uri + query
 			responses.push(response)
 		elsif query_type == :keyword
