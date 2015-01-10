0bd876701fb33a7a7f25691edc7deb95e6eb9ef4
cjwind <cwentsai@gmail.com>
2015-01-10 14:51:33 +0800

    add test case for search title by google

---
+++ test/tc_search_title_by_google_api.rb
@@ -0,0 +1,25 @@
+#encoding: utf-8
+
+require 'test/unit'
+require './booksr'
+require './book'
+
+class TestSearchTitleByGoogleApi1 < Test::Unit::TestCase
+	def setup	# will be called before run each member function
+		@books = Booksr.search("時間簡史", :title, [:google])
+	end
+
+	def test_result_count
+		assert_equal(9, @books.size)
+	end
+end
+
+class TestSearchTitleByGoogleApi2 < Test::Unit::TestCase
+	def setup
+		@books = Booksr.search("Ruby on Rails", :title, [:google])
+	end
+
+	def test_result_count_when_over_40
+		assert_equal(172, @books.size)
+	end
+end
