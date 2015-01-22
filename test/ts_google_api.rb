d8ce4878931dd86be129608b4123c10f438461b6
cjwind <cwentsai@gmail.com>
2015-01-22 20:59:36 +0800

    require rubygems and bundler/setup in test

--- test/ts_google_api.rb
+++ test/ts_google_api.rb
@@ -1,5 +1,7 @@
 # Search by Google Book API test suite
 
+require 'rubygems'
+require 'bundler/setup'
 require 'test/unit/testsuite'
 require './test/tc_search_isbn'
 require './test/tc_search_title'
